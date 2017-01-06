4caf93496d8929345e68f04fd9c0904631eb72e7
Will Hegedus <wbhegedus@gmail.com>
2017-01-06 09:24:49 -0500

    added try/except for Add Punch confirmation dialog

--- timesaver_io.py
+++ timesaver_io.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 username = sys.argv[1]
 password = sys.argv[2]
@@ -20,5 +21,12 @@ passElem.send_keys(Keys.RETURN)
 WebDriverWait(driver, 10).until(EC.title_contains(username))
 punchIt = driver.find_element_by_id("bttAddPunch")
 punchIt.click()
+
+try:
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located(By.ID, "FloatMsgBtn0"))
+    acceptIt = driver.find_element_by_id("FloatMsgBtn0")
+    acceptIt.click()
+except:
+    pass
 
 driver.close()
